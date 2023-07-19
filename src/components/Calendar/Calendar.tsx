f1c2f1b505157e63556c791a8bd38b04b3b4db02
Ly Anh Kiet <lyanhkiet2111@gmail.com>
2023-07-19 14:57:27 +0700

    Add modal (Still need fix)

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -5,52 +5,68 @@ import CustomToolbar from "./customToolbar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import useCalendarData from "../../hooks/useCalendarData";
 import { IParamsGetCalendarDataPrisma } from "../../prisma/calendar";
+import {Dialog, DialogContentText} from "@mui/material";
 const localizer = dayjsLocalizer(dayjs);
 
 const CalendarContent = () => {
-  const [eventsData, setEventsData] = useState<any[]>([]);
-  const [paramsSearch, setParamsSearch] =
-    useState<IParamsGetCalendarDataPrisma>({
-      page: 1,
-      limit: 10,
-      keyword: "",
-    });
+    const [eventsData, setEventsData] = useState<any[]>([]);
+    const [open, setOpen] = useState(false);
+    const [paramsSearch, setParamsSearch] =
+        useState<IParamsGetCalendarDataPrisma>({
+            page: 1,
+            limit: 10,
+            keyword: "",
+        });
 
-  const { data } = useCalendarData(paramsSearch);
+    const { data } = useCalendarData(paramsSearch);
 
-  const handleSelect = ({ start, end }: any) => {
-    console.log(start);
-    console.log(end);
-    const title = window.prompt("New Event name");
-    if (title)
-      setEventsData([
-        ...eventsData,
-        {
-          start,
-          end,
-          title,
-        },
-      ]);
-  };
+    const handleSelect = ({ start, end }: any) => {
+        console.log(start);
+        console.log(end);
+        const title = window.prompt("New Event name");
+        if (title)
+            setEventsData([
+                ...eventsData,
+                {
+                    start,
+                    end,
+                    title,
+                },
+            ]);
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <div>
-      <Calendar
-        views={["day", "agenda", "week", "month"]}
-        selectable
-        localizer={localizer}
-        defaultDate={new Date()}
-        defaultView="month"
-        events={eventsData}
-        style={{ height: "100vh", width: "100%", backgroundColor: "white" }}
-        onSelectEvent={(event) => alert(event.title)}
-        onSelectSlot={handleSelect}
-        components={{
-          toolbar: CustomToolbar,
-        }}
-      />
-    </div>
-  );
+    const handleSelectedEvent = (event: any) => {
+        <Dialog open={open} onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description">
+            <DialogContentText id="alert-dialog-description">
+                {event.title}
+            </DialogContentText>
+        </Dialog>
+    };
+
+    return (
+        <div>
+            <Calendar
+                views={["day", "agenda", "week", "month"]}
+                selectable
+                localizer={localizer}
+                defaultDate={new Date()}
+                defaultView="month"
+                events={eventsData}
+                style={{ height: "100vh", width: "100%", backgroundColor: "white" }}
+                onSelectEvent={(event) => handleSelectedEvent(event)}
+                onSelectSlot={handleSelect}
+                components={{
+                    toolbar: CustomToolbar,
+                }}
+            />
+        </div>
+    );
 };
 
 export default CalendarContent;
