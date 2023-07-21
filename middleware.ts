57e9f9374a42bd0a979b57dd905c3a03aed84064
phamlam2k <mailam1309@gmail.com>
2023-07-21 23:04:47 +0700

    Fix

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/api")) {
-    return NextResponse.redirect(new URL("/", request.url));
+    // return NextResponse.redirect(new URL("/", request.url));
   }
 
   return NextResponse.next();
